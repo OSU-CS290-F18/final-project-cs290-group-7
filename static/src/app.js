cb67f51b62c2486aab7f9fc460f57c0a67ddd0ba
Andrew Quach <andrewquach10@gmail.com>
2018-11-17 19:41:18 -0800

    Add initial configurations for frontend stack

---
+++ static/src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './style.scss';
+
+export default class App extends React.Component {
+    render () {
+        return <p className="test">Hello World!!!</p>;
+    }
+}

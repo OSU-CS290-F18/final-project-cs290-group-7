899ed47533cc612872a559b00bbfcaccff450ec4
Andrew Quach <andrewquach10@gmail.com>
2018-11-19 00:53:12 -0800

    Add front page styling

--- static/src/routes.js
+++ static/src/routes.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router";
 
 import Header from "./components/Header/header";
+import FrontPage from "./components/front-page";
 
 const routes = (
     <div>
-        <Route exact path="/" component={Header} />
+        <Route exact path="/" component={FrontPage} />
         <Route exact path="/test" component={Header} />
     </div>
 )

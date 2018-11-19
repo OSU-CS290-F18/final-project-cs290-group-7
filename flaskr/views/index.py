eb3328bb1ead98275101f17ebd6ffaf86a9a4a9a
Stan Lyakhov <staslyakhov@gmail.com>
2018-11-18 19:37:24 -0800

    Fix a routing bug

--- flaskr/views/index.py
+++ flaskr/views/index.py
@@ -6,6 +6,7 @@ from .admin import admin
 def script(filename):
     return send_from_directory(os.path.join(admin.static_folder, "dist"), filename)
 
+@admin.route('/', methods=('GET',))
 @admin.route('/<path:filename>', methods=('GET',))
-def index(filename):
+def index(filename=None):
     return admin.send_static_file('index.html')

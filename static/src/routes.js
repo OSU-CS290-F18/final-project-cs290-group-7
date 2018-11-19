5e95261c49cff179e56e83c0e99aff422648988a
Andrew Quach <andrewquach10@gmail.com>
2018-11-19 01:51:18 -0800

    Add login and register pages

--- static/src/routes.js
+++ static/src/routes.js
@@ -3,10 +3,14 @@ import { Route, Switch } from "react-router";
 
 import Header from "./components/Header/header";
 import FrontPage from "./components/front-page";
+import LoginPage from "./components/login-page";
+import RegisterPage from "./components/register-page";
 
 const routes = (
     <div>
         <Route exact path="/" component={FrontPage} />
+        <Route exact path="/register" component={RegisterPage} />
+        <Route exact path="/login" component={LoginPage} />
         <Route exact path="/test" component={Header} />
     </div>
 )

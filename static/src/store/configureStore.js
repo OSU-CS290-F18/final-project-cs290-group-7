2796d519ab0a965c6c4c9889dabae2500780bb55
Andrew Quach <andrewquach10@gmail.com>
2018-11-20 04:53:14 -0800

    Add persistant storage

--- static/src/store/configureStore.js
+++ static/src/store/configureStore.js
@@ -1,19 +1,28 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import { createBrowserHistory } from "history";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import { routerMiddleware } from "connected-react-router";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "../reducers";
 
 const history = createBrowserHistory();
+const persistConfigs = {
+    key: "root",
+    storage: storage,
+}
+
 
 const configureStore = function configureStore(initialState) {
     const store = createStore(
-        rootReducer(history),
+        persistReducer(persistConfigs, rootReducer(history)),
         initialState,
         compose(
             applyMiddleware(
                 routerMiddleware(history),
                 thunkMiddleware,
+
             )
         )
     );
